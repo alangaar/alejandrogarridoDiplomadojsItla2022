652bf0fa903128293cbc3ee9566ae4b203a74ccc
alangaar <alangaar@gmail.com>
2022-01-26 10:52:11 -0400

    Update ejercicio.js
    practicas del diplomado

--- Clase3/ejercicio.js
+++ Clase3/ejercicio.js
@@ -8,11 +8,12 @@ var prompt = require('prompt');
   //
   // Get two properties from the user: username and email
   //
-  prompt.get(['username', 'email'], function (err, result) {
+  prompt.get(['nombre', 'email'], function (err, result) {
     //
     // Log the results.
     //
-    console.log('Command-line input received:');
-    console.log('  username: ' + result.username);
+    console.log('Datos Ingresados:');
+    console.log('  Nombre: ' + result.username);
     console.log('  email: ' + result.email);
+    console.log("Muchas Gracias");
 });
