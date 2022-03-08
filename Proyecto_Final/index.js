e41faf8eee6a0bbe57c305a76e3edb1108ae05bb
alangaar <alangaar@gmail.com>
2022-03-07 22:55:47 -0400

    Actualizacion Proyecto

--- Proyecto_Final/index.js
+++ Proyecto_Final/index.js
@@ -3,8 +3,8 @@ const app = express()
 const path = require('node:path');
 
 // archivos estaticos
-app.use(express.static('asset/'))
-app.use('librerias' ,express.static('node_modules'))
+app.use(express.static('assets/'))
+app.use('/librerias',express.static('node_modules'));
 
 
 // rutas para HTML
@@ -12,6 +12,7 @@ app.get('/', function(request, response) {
 response.sendFile(path.join(__dirname, '/vistas/index.html'));
 }) 
 
+//---------------------------------------------------------------------
 /*app.get('/tarjeta', function(request, response) { 
     response.sendFile(path.join(__dirname, '/vistas/tarjeta.html'));
 })
@@ -19,6 +20,7 @@ response.sendFile(path.join(__dirname, '/vistas/index.html'));
 app.get('/contacto', function(request, response) { 
     response.sendFile(path.join(__dirname, '/vistas/contacto.html'));
 })*/
+//---------------------------------------------------------------------
 
 // asignacion el puerto en el que correra la app
 app.listen(3000, function() {
